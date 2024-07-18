c61075309c61b471c68bf2a7c86d9d5f7f2bfd2d
fawzii15 <fawzio@live.fr>
2024-07-18 16:15:41 +0200

    Create main.py
    Main

---
+++ main.py
@@ -0,0 +1,17 @@
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
+from gui_app import GUIApp
+import sys
+import qdarkstyle
+
+def main():
+    app = QApplication(sys.argv)
+    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    window = GUIApp()
+    icon_path = "C:\\Users\\IM\\PycharmProjects\\TO-DO-LIST\\logo.png"
+    window.setWindowIcon(QIcon(icon_path))
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
